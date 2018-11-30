e9e387a5b9271ab533f13360d9dc1de82e94ae66
bishoybasily <bishoybasily@gmail.com>
2018-12-01 00:14:45 +0200

    testing logging

--- src/main/java/com/gmail/bishoybasily/hellogcp/HelloGcpApplication.java
+++ src/main/java/com/gmail/bishoybasily/hellogcp/HelloGcpApplication.java
@@ -6,9 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 @RestController
 @SpringBootApplication
 public class HelloGcpApplication {
+
+    private Logger logger = Logger.getLogger(getClass().getName());
 
     public static void main(String[] args) {
         SpringApplication.run(HelloGcpApplication.class, args);
@@ -19,6 +23,10 @@ public class HelloGcpApplication {
     @GetMapping
     public String hello(@RequestParam(required = false, value = "name") String name) {
         String respones = String.format(template, name == null ? "user" : name);
+
+        logger.info("Logging INFO with Logback");
+        logger.severe("Logging ERROR with Logback");
+
         return respones;
     }
 
