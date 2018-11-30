33f082468c0050d49ba74dd632d5bf9467f6c4e6
bishoybasily <bishoybasily@gmail.com>
2018-11-30 19:15:14 +0200

    everything seems happy, but still can't get fabric8 to build NodePort service

--- src/main/java/com/gmail/bishoybasily/hellogcp/HelloGcpApplication.java
+++ src/main/java/com/gmail/bishoybasily/hellogcp/HelloGcpApplication.java
@@ -3,6 +3,7 @@ package com.gmail.bishoybasily.hellogcp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,9 +14,12 @@ public class HelloGcpApplication {
         SpringApplication.run(HelloGcpApplication.class, args);
     }
 
+    private String template = "Hello, %s";
+
     @GetMapping
-    public String hello() {
-        return "hi";
+    public String hello(@RequestParam(required = false, value = "name") String name) {
+        String respones = String.format(template, name == null ? "user" : name);
+        return respones;
     }
 
 }
