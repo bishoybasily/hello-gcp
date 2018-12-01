8074678402c3b9669c52f18955285fdb7fbd1a0e
bishoybasily <bishoybasily@gmail.com>
2018-12-01 14:49:14 +0200

    testing logging

--- src/main/java/com/gmail/bishoybasily/hellogcp/HelloGcpApplication.java
+++ src/main/java/com/gmail/bishoybasily/hellogcp/HelloGcpApplication.java
@@ -1,11 +1,18 @@
 package com.gmail.bishoybasily.hellogcp;
 
+import com.google.cloud.ServiceOptions;
+import com.google.cloud.errorreporting.v1beta1.ReportErrorsServiceClient;
+import com.google.devtools.clouderrorreporting.v1beta1.ErrorContext;
+import com.google.devtools.clouderrorreporting.v1beta1.ProjectName;
+import com.google.devtools.clouderrorreporting.v1beta1.ReportedErrorEvent;
+import com.google.devtools.clouderrorreporting.v1beta1.SourceLocation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @RestController
@@ -26,6 +33,36 @@ public class HelloGcpApplication {
 
         logger.info("Logging INFO with Logback");
         logger.severe("Logging ERROR with Logback");
+
+
+        String projectId = ServiceOptions.getDefaultProjectId();
+        ProjectName projectName = ProjectName.of(projectId);
+
+        // Instantiate an Error Reporting Client
+        try (ReportErrorsServiceClient reportErrorsServiceClient = ReportErrorsServiceClient.create()) {
+
+            SourceLocation myMethod = SourceLocation.newBuilder()
+                    .setFilePath("Test.java")
+                    .setLineNumber(10)
+                    .setFunctionName("myMethod")
+                    .build();
+
+            ErrorContext errorContext = ErrorContext.newBuilder()
+                    .setReportLocation(myMethod)
+                    .build();
+
+            ReportedErrorEvent customErrorEvent = ReportedErrorEvent.getDefaultInstance()
+                    .toBuilder()
+                    .setMessage("custom error event")
+                    .setContext(errorContext)
+                    .build();
+
+            reportErrorsServiceClient.reportErrorEvent(projectName, customErrorEvent);
+
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Exception encountered logging custom event", e);
+        }
+
 
         return respones;
     }
